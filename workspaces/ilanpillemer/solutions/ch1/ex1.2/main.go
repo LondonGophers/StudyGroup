48964303908a6ba2da0416e752687c3c4177f82f
Ilan Pillemer <ilan.pillemer@gmail.com>
2019-02-06 21:49:28 +0000

    ex 1.2

---
+++ workspaces/ilanpillemer/solutions/ch1/ex1.2/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for i, v := range os.Args {
+		fmt.Printf("index[%d] value:[%s]\n", i, v)
+	}
+
+}
+
+//go run main.go 1 2 3
+//index[0] value:[/var/folders/cw/6_jr6ms12wgchzxj9s9kq5sm0000gn/T/go-build652312564/b001/exe/main]
+//index[1] value:[1]
+//index[2] value:[2]
+//index[3] value:[3]
