76db82f93e7ffb25b14a719623e89e7cd5941051
Ilan Pillemer <ilan.pillemer@gmail.com>
2019-02-07 22:23:21 +0000

    ex1.5

---
+++ workspaces/ilanpillemer/solutions/ch1/ex1.5/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var Green = color.RGBA{0, 230, 64, 1}
+
+//var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.Black, Green}
+
+const (
+	whiteIndex = 0
+	blackIndex = 1
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5
+		res     = 0.001
+		size    = 100
+		nframes = 64
+		delay   = 8
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
