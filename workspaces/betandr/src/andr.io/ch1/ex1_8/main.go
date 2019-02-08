e74316e1dd7031861cc7c528238bf451e90b40ce
Beth Anderson <bet@andr.io>
2019-02-08 17:06:02 +0000

    ex1_8

---
+++ workspaces/betandr/src/andr.io/ch1/ex1_8/main.go
@@ -0,0 +1,36 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 16.
+//!+
+
+// Fetch prints the content found at each specified URL.
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") { // ignoring https ;)
+			url = "http://" + url
+		}
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+//!-
