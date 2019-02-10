8f7f5d0e62e06931c20ef309dbafdc057db16155
Ilan Pillemer <ilan.pillemer@gmail.com>
2019-02-10 10:00:12 +0000

    ex2.2 use stdin if there are no arguments and gofmt

--- workspaces/ilanpillemer/solutions/ch2/ex2.2/main.go
+++ workspaces/ilanpillemer/solutions/ch2/ex2.2/main.go
@@ -28,16 +28,16 @@ func main() {
 	for w.Scan() {
 		args := strings.Fields(w.Text())
 		for _, arg := range args {
-		t, err := strconv.ParseFloat(arg, 64)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
-			os.Exit(1)
-		}
-		f := tempconv.Fahrenheit(t)
-		c := tempconv.Celsius(t)
-		fmt.Printf("%s = %s, %s = %s\n",
-			f, tempconv.FtoC(f), c, tempconv.CtoF(c))
+			t, err := strconv.ParseFloat(arg, 64)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+				os.Exit(1)
 			}
+			f := tempconv.Fahrenheit(t)
+			c := tempconv.Celsius(t)
+			fmt.Printf("%s = %s, %s = %s\n",
+				f, tempconv.FtoC(f), c, tempconv.CtoF(c))
+		}
 	}
 }
 
