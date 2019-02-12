92e3101cac85e758bce30430ed6d83c5310479ff
Beth Anderson <bet@andr.io>
2019-02-12 11:14:00 +0000

    ex5_1

---
+++ workspaces/betandr/src/andr.io/ch5/ex5_1/main.go
@@ -0,0 +1,43 @@
+// Change the findlinks program to traverse the `n.FirstChild` linked list using
+// recursive calls to `visit` instead of a loop.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks: %v\n", err)
+		os.Exit(1)
+	}
+
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+func visit(links []string, n *html.Node) []string {
+	// todo - write it out on the whiteboard
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+
+	if n.FirstChild != nil {
+		links = visit(links, n.FirstChild)
+	}
+
+	if n.NextSibling != nil {
+		links = visit(links, n.NextSibling)
+	}
+
+	return links
+}
