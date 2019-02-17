eac00c86968efe0372a1ac051e9db5fa6c2e8947
Beth Anderson <bet@andr.io>
2019-02-17 18:29:34 +0000

    ex7_6

--- workspaces/betandr/src/andr.io/ch7/ex7_6/main.go
+++ workspaces/betandr/src/andr.io/ch7/ex7_6/main.go
@@ -1,7 +1,7 @@
 // Add support for Kelvin temperatures to `tempFlag`.
 //
 // Explain why the help message contains °C when the default value of
-// `20.0` does not.
+// `20.0` does not:
 // It uses the `String()` method declared in `tempconv`
 // func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
 package main
