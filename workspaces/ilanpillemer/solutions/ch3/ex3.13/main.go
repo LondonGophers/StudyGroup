3b825ef8ce56a93f842a08f0e072a964a1e49990
Ilan Pillemer <ilan.pillemer@gmail.com>
2019-02-22 23:36:28 +0000

    ex3.13 test results

--- workspaces/ilanpillemer/solutions/ch3/ex3.13/main.go
+++ workspaces/ilanpillemer/solutions/ch3/ex3.13/main.go
@@ -25,3 +25,26 @@ const (
 	ZiB
 	YiB
 )
+
+
+//$ go test
+//KB 1000
+//MB 1000000
+//GB 1000000000
+//TB 1000000000000
+//PB 1000000000000000
+//EB 1000000000000000000
+//ZB 1e+21
+//YB 1e+24
+//
+//KiB 1024
+//MiB 1048576
+//GiB 1073741824
+//TiB 1099511627776
+//PiB 1125899906842624
+//EiB 1152921504606846976
+//ZiB 1.1805916207174113e+21
+//YiB 1.2089258196146292e+24
+//PASS
+//ok  	_/Users/ilanpillemer/git/study-group/workspaces/ilanpillemer/solutions/ch3/ex3.13	0.005s
+//$
