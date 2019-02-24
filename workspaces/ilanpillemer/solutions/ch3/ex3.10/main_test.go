e1455baf13db45d5f4fa0b649dc5b8a85da50938
Ilan Pillemer <ilan.pillemer@gmail.com>
2019-02-24 21:20:03 +0000

    remove test

--- workspaces/ilanpillemer/solutions/ch3/ex3.10/main_test.go
+++ workspaces/ilanpillemer/solutions/ch3/ex3.10/main_test.go
@@ -26,26 +26,5 @@ func TestComma(t *testing.T) {
 
 }
 
-//func TestBetandr(t *testing.T) {
-//
-//	tests := []struct {
-//		input string
-//		want  string
-//	}{
-//		{"123", "123"},
-//		{"12345", "12,345"},
-//		{"123456", "123,456"},
-//		{"1234567", "1,234,567"},
-//		{"12345678", "12,345,678"},
-//		{"123456789", "123,456,789"},
-//	}
-//
-//	for _, tst := range tests {
-//
-//		got := betandr(tst.input)
-//		if got != tst.want {
-//			t.Errorf("got %s, want %s", got, tst.want)
-//		}
-//	}
-//}
+
 
