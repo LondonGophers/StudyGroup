58060a8135cb73f54ad399a19b53ed6656666158
Ilan Pillemer <ilan.pillemer@gmail.com>
2019-02-24 21:13:11 +0000

    remove test

--- workspaces/ilanpillemer/solutions/ch4/ex4.4/main_test.go
+++ workspaces/ilanpillemer/solutions/ch4/ex4.4/main_test.go
@@ -56,22 +56,4 @@ func equals(left []int, right []int) bool {
 	return true
 }
 
-//func TestBetandr(t *testing.T) {
-//
-//	tests := []struct {
-//		input []int
-//		rot   int
-//		want  []int
-//	}{
-//		{[]int{0, 1, 2, 3, 4, 5}, 2, []int{2, 3, 4, 5, 0, 1}},
-//		{[]int{0, 1, 2, 3, 4, 5, 6}, 2, []int{2, 3, 4, 5, 6, 0, 1}},
-//		{[]int{0, 1, 2, 3, 4, 5, 6, 7}, 2, []int{2, 3, 4, 5, 6, 7, 0, 1}},
-//		{[]int{0, 1, 2, 3, 4, 5, 6, 7}, 3, []int{3, 4, 5, 6, 7, 0, 1, 2}},
-//	}
-//	for _, z := range tests {
-//		betandr(z.rot, z.input)
-//		if !equals(z.input, z.want) {
-//			t.Errorf("want %#v, got %#v\n", z.want, z.input)
-//		}
-//	}
-//}
+
