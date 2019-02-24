3bb3569aa65147b787dde8f9fd5ba3872587e9c5
Ilan Pillemer <ilan.pillemer@gmail.com>
2019-02-24 18:33:21 +0000

    ex4.4 rotate in a single pass

--- workspaces/ilanpillemer/solutions/ch4/ex4.4/main.go
+++ workspaces/ilanpillemer/solutions/ch4/ex4.4/main.go
@@ -22,9 +22,3 @@ func rotate(input []int, rot int) []int {
 	return rotateRight(input, len(input)-rot) // moving left equivalent in terms of moving right
 }
 
-// reverse reverses a slice of ints in place.
-func reverse(s []int) {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
-	}
-}
