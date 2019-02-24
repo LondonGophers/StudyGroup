30500a9bf3b4f8d1db33a991315fc4e86434d612
Ilan Pillemer <ilan.pillemer@gmail.com>
2019-02-24 18:51:57 +0000

    ex4.4 rotate in a single pass

--- workspaces/ilanpillemer/solutions/ch4/ex4.4/main.go
+++ workspaces/ilanpillemer/solutions/ch4/ex4.4/main.go
@@ -1,5 +1,11 @@
 package main
 
+// this does in one pass but returns a new slice.
+// it is possible to do in one pass and in place.
+// this would require doing a tag replacement process
+// with a temp holder in between. ie you keep replacing
+// the one replaced until all have been replaced.
+
 // rotate operates in a single pass
 // in order to do that the values of the slice are stored
 // elsewhere so that values that are overwritten are not lost
@@ -21,4 +27,3 @@ func rotateRight(input []int, rot int) []int {
 func rotate(input []int, rot int) []int {
 	return rotateRight(input, len(input)-rot) // moving left equivalent in terms of moving right
 }
-
