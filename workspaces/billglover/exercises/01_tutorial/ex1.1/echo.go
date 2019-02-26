3cb9fe2c55f33f4429f570b4b7ab9fe39083d261
Bill Glover <bill@billglover.co.uk>
2019-02-26 12:40:21 +0000

    Solve ex1.1

---
+++ workspaces/billglover/exercises/01_tutorial/ex1.1/echo.go
@@ -0,0 +1,15 @@
+/*
+echo prints the name of the command and the command line arguments
+passed to it.
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Join(os.Args, " "))
+}
