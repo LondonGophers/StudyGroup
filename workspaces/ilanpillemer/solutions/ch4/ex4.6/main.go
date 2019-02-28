c54d2ba0b45abd230ced91ec8961a40bb4019ead
Ilan Pillemer <ilan.pillemer@gmail.com>
2019-02-28 23:28:26 +0000

    ex4.6 passing all tests

--- workspaces/ilanpillemer/solutions/ch4/ex4.6/main.go
+++ workspaces/ilanpillemer/solutions/ch4/ex4.6/main.go
@@ -5,38 +5,42 @@ import (
 	"unicode/utf8"
 )
 
+//squashing spaces is just a special case of deuplicating with two
+//differences.
+//firstly it must only consider spaces
 func squash(s *[]byte) {
 	b := *s
-	todo := len(b) + 1 // when we are done there wont be anything todo
-	start, end := 0, 0 // unicode run that needs to be replaced with one space
-	inSpace := false   // state
-	elided := 0        // how much to chop off the end when we are done
-	for todo > 0 {
-		r, l := utf8.DecodeRune(b[end:])
-		if unicode.IsSpace(r) {
-			if inSpace {
-				end += l // move end pointer
-			} else {
-				start = end // set begin pointer
-				end += l
+	end := len(b)
+	for i := len(b) - 1; i > 0; i-- {
+		lr, ln := utf8.DecodeLastRune(b[:i+1])
+		rr, _ := utf8.DecodeLastRune(b[:i+1-ln])
+
+		if !unicode.IsSpace(lr) && !unicode.IsSpace(rr) {
+			// they are not equal.. dont do anything..
+		}
+		if unicode.IsSpace(lr) && unicode.IsSpace(rr) {
+			// they are equal copy all from the right left the
+			// length of the rightmost rune
+			for k := 0; k < ln; k++ {
+				for j := i - 1; j < len(b)-1; j++ {
+					b[j] = b[j+1]
+				}
+				// decrement end pointer
+				end--
 			}
-			inSpace = true
-			todo = todo - l
-			elided += l // more to chop
-			continue
 		}
-		if inSpace {
-			b[start] = ' '
-			elided--                   // account for the single space that replaces a run when chopping
-			copy(b[start+1:], b[end:]) // replace run with space
-			todo = todo - l
-			end = start + l // reset pointers
-			inSpace = false
-			continue
+		// if its the last space in the run, change it to an ascii space
+		// and shift left if necessary.
+		// this will be one less as we want to keep one byte.
+		if unicode.IsSpace(lr) && !unicode.IsSpace(rr) && lr != 32 {
+			b[i] = ' '
+			for k := 0; k < ln-1; k++ {
+				for j := i - 1; j < len(b)-1; j++ {
+					b[j] = b[j+1]
+				}
+				end--
+			}
 		}
-		todo = todo - l
-		end = end + l // shift pointer
 	}
-	b = b[:len(b)-elided] //cut off end
-	*s = b
+	*s = b[:end]
 }
--- workspaces/ilanpillemer/solutions/ch4/ex4.6/main_test.go
+++ workspaces/ilanpillemer/solutions/ch4/ex4.6/main_test.go
@@ -15,27 +15,26 @@ func Test(t *testing.T) {
 		want  []byte
 	}{
 		{[]byte{'a', '\t', 0xc2, 0x85, 0xc2, 0xa0, 'b'}, []byte{'a', ' ', 'b'}},
+		{[]byte("abc"), []byte("abc")},
+		{[]byte(" abc"), []byte(" abc")},
+		{[]byte(" abc               "), []byte(" abc ")},
+		{[]byte{'a', ' ', 0xc2, 0x85, 0xc2, 0xa0, 'b', 'c'}, []byte{'a', ' ', 'b', 'c'}},
 		{[]byte{'a', '\t', 0xc2, 0x85, 0xc2, 0xa0, 'b', 'c'}, []byte{'a', ' ', 'b', 'c'}},
 		{[]byte{'a', 0xc2, 0x85, 0xc2, 0xa0, 'b', 'c'}, []byte{'a', ' ', 'b', 'c'}},
 		{[]byte{'a', '\t', 0xc2, 0x85, 'b', 0xc2, 0xa0, 'b', 'c'}, []byte{'a', ' ', 'b', ' ', 'b', 'c'}},
 		{[]byte{'a', '\t', 0xc2, 0x85, 'b', 0xc2, 0xa0, 'b', 'c', 'c'}, []byte{'a', ' ', 'b', ' ', 'b', 'c', 'c'}},
+		{[]byte("a b c d e"), []byte("a b c d e")},
+		{[]byte("a  b    c     d      e"), []byte("a b c d e")},
+		{[]byte("Finally,  as    the sky      began   to    grow           light"), []byte("Finally, as the sky began to grow light")},
+		{[]byte("Finally,  as    the sky      began   to"), []byte("Finally, as the sky began to")},
 	}
 
 	for _, z := range tests {
 		squash(&z.input)
 		if !bytes.Equal(z.input, z.want) {
-			t.Errorf("got [%#v], want [%#v]\n", string((z.input)), string(z.want))
+			t.Errorf("squash: got [%#v], want [%#v]\n", string((z.input)), string(z.want))
 		}
-
 	}
-
 }
 
-//func TestStuff(t *testing.T) {
-//	fmt.Printf("U+0085 -> %b and is space? :%t \n", rune(0x85), unicode.IsSpace(rune(0x85)))
-//	fmt.Printf("U+00A0 -> %b and is space? :%t \n", rune(0xA0), unicode.IsSpace(rune(0xA0)))
-//	fmt.Printf("0xc2 0x85 -> %b %b \n", byte(0xc2), byte(0x85))
-//	r := []rune{'a', '\t', 0x85, 0xA0, 'b'}
-//
-//	fmt.Printf("%#v\n", []byte(string(r)))
-//}
+
