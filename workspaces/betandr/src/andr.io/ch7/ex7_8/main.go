87b0786cba19243b067733a2f8988f1030bc1159
betandr <bet@andr.io>
2019-03-01 12:24:36 +0000

    ex7_8

--- workspaces/betandr/src/andr.io/ch7/ex7_8/main.go
+++ workspaces/betandr/src/andr.io/ch7/ex7_8/main.go
@@ -7,6 +7,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"text/tabwriter"
 	"time"
@@ -25,6 +26,7 @@ var tracks = []*music.Track{
 	{Title: "Boys Say Go!", Artist: "Depeche Mode", Album: "Speak & Spell", Year: 1981, Length: length("3m06s")},
 	{Title: "Go With The Flow", Artist: "Queens Of The Stone Age", Album: "Songs For The Deaf", Year: 2002, Length: length("3m08s")},
 	{Title: "Go", Artist: "blink-182", Album: "blink-182", Year: 2003, Length: length("1m53s")},
+	{Title: "Blitzkrieg Bop", Artist: "Ramones", Album: "Ramones", Year: 1976, Length: length("2m12s")},
 	{Title: "Go Your Own Way", Artist: "Fleetwood Mac", Album: "Rumours", Year: 1977, Length: length("3m43s")},
 }
 
@@ -36,7 +38,7 @@ func length(s string) time.Duration {
 	return dur
 }
 
-func printPlaylist(pl *music.Playlist) {
+func printPlaylist(pl *music.Playlist, order []music.Attribute) {
 	const format = "%v\t%v\t%v\t%v\t%v\t\n"
 	tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
 	fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
@@ -50,8 +52,26 @@ func printPlaylist(pl *music.Playlist) {
 func main() {
 	pl := new(music.Playlist)
 	pl.Tracks = tracks
-	order := []music.Attribute{music.Title, music.Artist, music.Album, music.Year, music.Length}
 
-	pl.OrderBy(order)
-	printPlaylist(pl)
+	order := []music.Attribute{music.Year, music.Title, music.Artist, music.Album, music.Length}
+	shuffled := make([]music.Attribute, 0)
+
+	// shuffle the order for the demo
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for _, i := range r.Perm(len(order)) {
+		shuffled = append(shuffled, order[i])
+	}
+
+	pl.OrderBy(shuffled)
+	printPlaylist(pl, shuffled)
+
+	fmt.Printf("[sort order: %s, %s, %s, %s, %s]\n",
+		music.Heading(shuffled[0]),
+		music.Heading(shuffled[1]),
+		music.Heading(shuffled[2]),
+		music.Heading(shuffled[3]),
+		music.Heading(shuffled[4]),
+	)
+
+	// Conversely `sort.Stable` sorts by a column while keeping the original order of equal elements.
 }
