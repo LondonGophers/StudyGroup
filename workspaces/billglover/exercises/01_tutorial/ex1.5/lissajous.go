068f6e552c0fa1b11a3ed218b2da60421924f526
Bill Glover <bill@billglover.co.uk>
2019-03-05 11:07:17 +0000

    Solve ex1.5

---
+++ workspaces/billglover/exercises/01_tutorial/ex1.5/lissajous.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var pallette = []color.Color{color.Black, color.RGBA{0x00, 0xFF, 0x00, 0xFF}}
+
+const (
+	blackIndex = 0
+	greenIndex = 1
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 200   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, pallette)
+
+		for t := 0.0; t < cycles*2.0*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	err := gif.EncodeAll(out, &anim)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "unable to encode gif:", err)
+	}
+}
