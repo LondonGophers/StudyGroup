f041d0bdb103c3df5723e6d97482a0ba4f5c7d35
Beth Anderson <bet@andr.io>
2019-03-07 11:49:01 +0000

    ex7_11

--- workspaces/betandr/src/andr.io/ch7/ex7_11/main.go
+++ workspaces/betandr/src/andr.io/ch7/ex7_11/main.go
@@ -17,20 +17,23 @@ import (
 // curl -X GET http://localhost:8000/update?item=socks&price=6
 // curl -X GET http://localhost:8000/price\?item\=socks
 // curl -X GET http://localhost:8000/create\?item\=sandals\&price\=20
-// curl -X GET http://localhost:8000/list
+// curl -X GET http://localhost:8000/show\?item\=sandals
 // curl -X GET http://localhost:8000/delete\?item\=sandals
 
 func main() {
 	db := database{"shoes": 50, "socks": 5}
 	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/show", db.read)
 	http.HandleFunc("/price", db.price)
 	http.HandleFunc("/update", db.update)
+	http.HandleFunc("/create", db.create)
+	http.HandleFunc("/delete", db.delete)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 type dollars float32
 
-func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
+func (d dollars) String() string { return fmt.Sprintf("£%.2f", d) }
 
 type database map[string]dollars
 
@@ -51,11 +54,32 @@ func (db database) price(w http.ResponseWriter, req *http.Request) {
 }
 
 func (db database) create(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "TODO\n")
+	item := req.URL.Query().Get("item")
+	if _, ok := db[item]; ok {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "item exists: %s\n", item)
+		return
+	}
+	p := req.URL.Query().Get("price")
+	price, err := strconv.ParseFloat(p, 10)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "invalid price: %s\n", p)
+		return
+	}
+	db[item] = dollars(price)
+	fmt.Fprintf(w, "%s: %s\n", item, db[item])
 }
 
 func (db database) read(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "TODO\n")
+	item := req.URL.Query().Get("item")
+
+	if _, ok := db[item]; ok {
+		fmt.Fprintf(w, "%s: %s\n", item, db[item])
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+	}
 }
 
 func (db database) update(w http.ResponseWriter, req *http.Request) {
@@ -65,6 +89,7 @@ func (db database) update(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "invalid price: %s\n", p)
+		return
 	}
 
 	if _, ok := db[item]; ok {
@@ -77,5 +102,15 @@ func (db database) update(w http.ResponseWriter, req *http.Request) {
 }
 
 func (db database) delete(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "TODO\n")
+	item := req.URL.Query().Get("item")
+
+	if _, ok := db[item]; ok {
+		delete(db, item)
+		if _, ok := db[item]; !ok {
+			fmt.Fprintf(w, "deleted item: %q\n", item)
+		}
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "no such item: %q\n", item)
+	}
 }
