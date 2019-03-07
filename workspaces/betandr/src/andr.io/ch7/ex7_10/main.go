637dfad2c46ddeb683daadcdc13d2cbf2d01f03e
Beth Anderson <bet@andr.io>
2019-03-07 15:06:05 +0000

    fix ex7_10

--- workspaces/betandr/src/andr.io/ch7/ex7_10/main.go
+++ workspaces/betandr/src/andr.io/ch7/ex7_10/main.go
@@ -14,16 +14,23 @@ import (
 // reversing the sequence would not change it
 func IsPalindrome(s sort.Interface) bool {
 	for i, j := 0, s.Len()-1; i < s.Len()-1/2; i, j = i+1, j-1 {
-		if !s.Less(i, j) && !s.Less(j, i) {
-			return true
+		if !equals(s, i, j) {
+			return false
 		}
 	}
-	return false
+	return true
+}
+
+func equals(s sort.Interface, i, j int) bool {
+	return !s.Less(i, j) && !s.Less(j, i)
 }
 
 func main() {
 	sequence := sort.StringSlice([]string{"p", "a", "l", "a", "m", "i", "n", "o"})
 	fmt.Printf("%v => %t\n", sequence, IsPalindrome(sequence))
+
+	nums := sort.IntSlice([]int{1, 3, 5, 2, 5, 3, 1})
+	fmt.Printf("%v => %t\n", nums, IsPalindrome(nums))
 
 	palindrome := sort.StringSlice([]string{"t", "a", "c", "o", "c", "a", "t"})
 	fmt.Printf("%v   => %t\n", palindrome, IsPalindrome(palindrome))
