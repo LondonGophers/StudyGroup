77c7767bafeaa70ebc391f43bf0e3c4273c84dc8
Bill Glover <bill@billglover.co.uk>
2019-03-11 08:36:52 +0000

    Solve ex2.3

---
+++ workspaces/billglover/exercises/02_program_structure/ex2.3/popcount.go
@@ -0,0 +1,28 @@
+package popcount
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func popCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func popCountByteShift(x uint64) int {
+	c := 0
+	for b := uint8(0); b < 64; b += 8 {
+		c += int(pc[byte(x>>b)])
+	}
+	return c
+}
---
+++ workspaces/billglover/exercises/02_program_structure/ex2.3/popcount_test.go
@@ -0,0 +1,45 @@
+package popcount
+
+import (
+	"math"
+	"testing"
+)
+
+var cases = []struct {
+	i uint64
+	o int
+}{
+	{i: 0, o: 0},
+	{i: 1, o: 1},
+	{i: 2, o: 1},
+	{i: 3, o: 2},
+	{i: math.MaxUint64, o: 64},
+}
+
+func TestPopCount(t *testing.T) {
+	for _, tc := range cases {
+		if got, want := popCount(tc.i), tc.o; got != want {
+			t.Errorf("test: %d, got: %d, want: %d", tc.i, got, want)
+		}
+	}
+}
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popCount(math.MaxUint64)
+	}
+}
+
+func TestPopCountByteShift(t *testing.T) {
+	for _, tc := range cases {
+		if got, want := popCountByteShift(tc.i), tc.o; got != want {
+			t.Errorf("test: %d, got: %d, want: %d", tc.i, got, want)
+		}
+	}
+}
+
+func BenchmarkPopCountByteShift(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popCountByteShift(math.MaxUint64)
+	}
+}
