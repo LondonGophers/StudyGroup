5ba91418c5d3bcc49d6338e4ead12e2b61017ce2
betandr <bet@andr.io>
2019-04-25 19:45:19 +0100

    ex7_16

--- workspaces/betandr/src/andr.io/ch7/ex7_16/main.go
+++ workspaces/betandr/src/andr.io/ch7/ex7_16/main.go
@@ -2,11 +2,15 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"path/filepath"
+	"strconv"
 	"text/template"
+
+	"andr.io/ch7/ex7_15/eval"
 )
 
 func main() {
@@ -14,7 +18,27 @@ func main() {
 		renderCalculator(w)
 	}
 
+	evaluateHandler := func(w http.ResponseWriter, r *http.Request) {
+		e := r.URL.Query().Get("e")
+
+		expr, err := eval.Parse(e)
+		if err != nil {
+			fmt.Fprintf(w, "%v", err)
+		} else {
+			vars := make(map[eval.Var]bool)
+			err = expr.Check(vars)
+			if err != nil {
+				fmt.Fprintf(w, "%v", err)
+			} else {
+				env := make(map[eval.Var]float64)
+				result := strconv.FormatFloat(expr.Eval(env), 'f', -1, 64)
+				fmt.Fprintf(w, result)
+			}
+		}
+	}
+
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/eval", evaluateHandler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 	return
 }
