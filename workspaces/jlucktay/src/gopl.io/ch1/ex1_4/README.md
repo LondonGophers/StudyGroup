c3964b43e9574b5f30e380e8d913948ad6bf977d
James Lucktaylor <jlucktay+github@gmail.com>
2019-05-05 06:18:46 +0100

    Complete exercise 1.4.
    Added some logging and an extra layer to the map where results are
    stored, to capture and expose all files with multiple occurrences of
    lines.

---
+++ workspaces/jlucktay/src/gopl.io/ch1/ex1_4/README.md
@@ -0,0 +1,3 @@
+# Exercise 1.4
+
+Modify `dup2` to print the names of all files in which each duplicated line occurs.
---
+++ workspaces/jlucktay/src/gopl.io/ch1/ex1_4/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	counts := make(map[string]map[string]int)
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		log.Fatal("Must provide at least one filename as an argument.")
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				log.WithFields(log.Fields{
+					"error":    err,
+					"filename": arg,
+				}).Error("Error opening a file.")
+				continue
+			}
+
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, m := range counts {
+		total := 0
+		filenames := make([]string, 0)
+
+		for file, matchCount := range m {
+			filenames = append(filenames, file)
+			total += matchCount
+		}
+
+		if total > 1 {
+			log.WithFields(log.Fields{
+				"files": strings.Join(filenames, ", "),
+				"line":  line,
+				"total": total,
+			}).Info("Files in which this duplicated line occurs.")
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]map[string]int) {
+	s, err := f.Stat()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+			"file":  f,
+		}).Error("Couldn't stat file.")
+		return
+	}
+
+	input := bufio.NewScanner(f)
+
+	for input.Scan() {
+		if _, exists := counts[input.Text()]; !exists {
+			log.WithFields(log.Fields{
+				"key": input.Text(),
+			}).Trace("Initialising sub-map within 'counts'.")
+
+			counts[input.Text()] = make(map[string]int)
+		}
+		counts[input.Text()][s.Name()]++
+	}
+}
