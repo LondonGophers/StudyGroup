8700bdf28636071188fae515e4b651080828d958
James Lucktaylor <jlucktay+github@gmail.com>
2019-05-05 07:44:41 +0100

    Complete exercise 1.8.
    Threw HTTPS into the mix as well, because why not.

---
+++ workspaces/jlucktay/src/gopl.io/ch1/ex1_8/README.md
@@ -0,0 +1,4 @@
+# Exercise 1.8
+
+Modify `fetch` to add the prefix `http://` to each argument URL, if it is missing. You might want to use
+`strings.HasPrefix`.
---
+++ workspaces/jlucktay/src/gopl.io/ch1/ex1_8/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") && !strings.HasPrefix(url, "https://") {
+			url = "https://" + url
+		}
+		resp, errGet := http.Get(url)
+		if errGet != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", errGet)
+			os.Exit(1)
+		}
+		defer resp.Body.Close()
+
+		_, errCopy := io.Copy(os.Stdout, resp.Body)
+		if errCopy != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, errCopy)
+			os.Exit(1)
+		}
+	}
+}
