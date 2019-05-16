71f5def1fad717beae547cae18db41ee6a7b5af9
James Lucktaylor <jlucktay+github@gmail.com>
2019-05-16 12:23:51 +0100

    Add Kelvin into the mix.

--- workspaces/jlucktay/ch2/ex_2_1/conv.go
+++ workspaces/jlucktay/ch2/ex_2_1/conv.go
@@ -5,3 +5,15 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
 // FToC converts a Fahrenheit temperature to Celsius.
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+// CToK converts a Celsius temperature to Kelvin.
+func CToK(c Celsius) Kelvin { return Kelvin(c - 273.15) }
+
+// KToC converts a Kelvin temperature to Celsius.
+func KToC(k Kelvin) Celsius { return Celsius(k + 273.15) }
+
+// FToK converts a Fahrenheit temperature to Kelvin.
+func FToK(f Fahrenheit) Kelvin { return Kelvin((f - (32 + 273.15)) * 5 / 9) }
+
+// KToF converts a Kelvin temperature to Fahrenheit.
+func KToF(k Kelvin) Fahrenheit { return Fahrenheit(k*9/5 + 32 + 273.15) }
--- workspaces/jlucktay/ch2/ex_2_1/tempconv.go
+++ workspaces/jlucktay/ch2/ex_2_1/tempconv.go
@@ -1,16 +1,22 @@
-// Package tempconv performs Celsius and Fahrenheit conversions.
+// Package tempconv performs Celsius, Fahrenheit, and Kelvin conversions.
 package tempconv
 
 import "fmt"
 
 type Celsius float64
 type Fahrenheit float64
+type Kelvin float64
 
 const (
 	AbsoluteZeroC Celsius = -273.15
 	FreezingC     Celsius = 0
 	BoilingC      Celsius = 100
+
+	AbsoluteZeroK Kelvin = 0
+	FreezingK     Kelvin = 273.15
+	BoilingK      Kelvin = 373.15
 )
 
 func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func (k Kelvin) String() string     { return fmt.Sprintf("%gK", k) }
