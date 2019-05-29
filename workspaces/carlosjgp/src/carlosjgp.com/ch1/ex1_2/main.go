a59e4440af8305327414acf222097bf360f58076
Carlos Juan Gómez Peñalver <carlosjuangp@gmail.com>
2019-05-29 20:19:07 +0100

    feat(ch1/ex1_2) Add Chapter 1 ex. 1.2 for carlosjgp

---
+++ workspaces/carlosjgp/src/carlosjgp.com/ch1/ex1_2/main.go
@@ -0,0 +1,23 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 4.
+//!+
+
+// Echo1 prints its command-line arguments.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var s string
+	for i := 1; i < len(os.Args); i++ {
+		s += fmt.Sprintf("%d: %s \n", i, os.Args[i])
+	}
+	fmt.Println(s)
+}
+
+//!-
