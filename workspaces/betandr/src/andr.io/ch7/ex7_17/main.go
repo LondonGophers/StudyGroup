6f3f30d084a631ba218d55fad95ecef0a81cb599
betandr <bet@andr.io>
2019-06-24 07:46:47 +0100

    ex7_17 wip

---
+++ workspaces/betandr/src/andr.io/ch7/ex7_17/main.go
@@ -0,0 +1,54 @@
+// Extend `xmlselect` so that elements may be selected not just by name, but by
+// their attributes too, in the manner of CSS, so that, for instance, an element
+// like `<div id="page" class="wide">` could be selected by matching `id` or
+// `class` as well as its name.
+//
+// WORK IN PROGRESS
+// fetch http://www.w3.org/TR/2006/REC-xml11-20060816 | go run andr.io/ch7/ex7_17 div div h2
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+func main() {
+	dec := xml.NewDecoder(os.Stdin)
+	var stack []string // stack of element names
+	for {
+		tok, err := dec.Token()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			fmt.Fprintf(os.Stderr, "xmlselect: %v\n", err)
+			os.Exit(1)
+		}
+		switch tok := tok.(type) {
+		case xml.StartElement:
+			stack = append(stack, tok.Name.Local) // push
+		case xml.EndElement:
+			stack = stack[:len(stack)-1] // pop
+		case xml.CharData:
+			if containsAll(stack, os.Args[1:]) {
+				fmt.Printf("%s: %s\n", strings.Join(stack, " "), tok)
+			}
+		}
+	}
+}
+
+// containsAll reports whether x contains the elements of y, in order.
+func containsAll(x, y []string) bool {
+	for len(y) <= len(x) {
+		if len(y) == 0 {
+			return true
+		}
+		if x[0] == y[0] {
+			y = y[1:]
+		}
+		x = x[1:]
+	}
+	return false
+}
