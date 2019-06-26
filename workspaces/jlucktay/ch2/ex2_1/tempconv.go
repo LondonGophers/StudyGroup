1eca2460f4bca3bb231f3c5fe7c72e0cbc28cd0f
James Lucktaylor <jlucktay+github@gmail.com>
2019-06-26 07:02:05 +0100

    Add F consts.

--- workspaces/jlucktay/ch2/ex2_1/tempconv.go
+++ workspaces/jlucktay/ch2/ex2_1/tempconv.go
@@ -16,6 +16,10 @@ const (
 	FreezingC     Celsius = 0
 	BoilingC      Celsius = 100
 
+	AbsoluteZeroF Fahrenheit = -459.67
+	FreezingF     Fahrenheit = 32
+	BoilingF      Fahrenheit = 212
+
 	AbsoluteZeroK Kelvin = 0
 	FreezingK     Kelvin = 273.15
 	BoilingK      Kelvin = 373.15
