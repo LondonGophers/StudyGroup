d852c227900483e75d392fbfaed5481b9f1a318a
James Lucktaylor <jlucktay+github@gmail.com>
2019-06-28 17:44:32 +0100

    Complete 2.2.

--- workspaces/jlucktay/ch2/ex2_2/main.go
+++ workspaces/jlucktay/ch2/ex2_2/main.go
@@ -12,16 +12,24 @@ import (
 func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(flag.CommandLine.Output(), "Available commands for %s:\n", os.Args[0])
-		fmt.Fprintf(flag.CommandLine.Output(), "  length         Feet <-> Meters\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "  length         Feet <-> Metres\n")
 		fmt.Fprintf(flag.CommandLine.Output(), "  temperature    Celsius <-> Fahrenheit <-> Kelvin\n")
 		fmt.Fprintf(flag.CommandLine.Output(), "  weight         Pounds <-> Kilograms\n")
 		flag.PrintDefaults()
 	}
 
+	lengthCommand := flag.NewFlagSet("length", flag.ExitOnError)
+	feetFlag := lengthCommand.Float64("feet", 0, "a length value in ft")
+	metresFlag := lengthCommand.Float64("metres", 0, "a length value in m")
+
 	temperatureCommand := flag.NewFlagSet("temperature", flag.ExitOnError)
 	celsiusFlag := temperatureCommand.Float64("celsius", 0, "a temperature value in °C")
 	fahrenheitFlag := temperatureCommand.Float64("fahrenheit", 0, "a temperature value in °F")
 	kelvinFlag := temperatureCommand.Float64("kelvin", 0, "a temperature value in K")
+
+	weightCommand := flag.NewFlagSet("weight", flag.ExitOnError)
+	kilogramsFlag := weightCommand.Float64("kilograms", 0, "a weight value in kg")
+	poundsFlag := weightCommand.Float64("pounds", 0, "a weight value in lb")
 
 	if len(os.Args) < 2 {
 		flag.Usage()
@@ -29,12 +37,41 @@ func main() {
 	}
 
 	switch os.Args[1] {
+	case lengthCommand.Name():
+		if errParse := lengthCommand.Parse(os.Args[2:]); errParse != nil {
+			log.Fatalf("error parsing length flags: %v", errParse)
+		}
 	case temperatureCommand.Name():
 		if errParse := temperatureCommand.Parse(os.Args[2:]); errParse != nil {
 			log.Fatalf("error parsing temperature flags: %v", errParse)
 		}
+	case weightCommand.Name():
+		if errParse := weightCommand.Parse(os.Args[2:]); errParse != nil {
+			log.Fatalf("error parsing weight flags: %v", errParse)
+		}
 	default:
-		log.Fatalf("'%v' is not a valid command.\n", os.Args[1])
+		fmt.Printf("'%v' is not a valid command.\n", os.Args[1])
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if lengthCommand.Parsed() {
+		if lengthCommand.NFlag() == 0 {
+			fmt.Println("Please specify a length to convert.")
+			lengthCommand.PrintDefaults()
+			os.Exit(1)
+		}
+
+		lengthCommand.Visit(func(f *flag.Flag) {
+			switch f.Name {
+			case "feet":
+				feet := *feetFlag
+				fmt.Printf("%vft is %vm.\n", feet, feet*3.281)
+			case "metres":
+				metres := *metresFlag
+				fmt.Printf("%vm is %vft.\n", metres, metres/3.281)
+			}
+		})
 	}
 
 	if temperatureCommand.Parsed() {
@@ -55,6 +92,25 @@ func main() {
 			case "kelvin":
 				kelvin := tempconv.Kelvin(*kelvinFlag)
 				fmt.Printf("%v is %v and %v.\n", kelvin, tempconv.KToC(kelvin), tempconv.KToF(kelvin))
+			}
+		})
+	}
+
+	if weightCommand.Parsed() {
+		if weightCommand.NFlag() == 0 {
+			fmt.Println("Please specify a weight to convert.")
+			weightCommand.PrintDefaults()
+			os.Exit(1)
+		}
+
+		weightCommand.Visit(func(f *flag.Flag) {
+			switch f.Name {
+			case "kilograms":
+				kilograms := *kilogramsFlag
+				fmt.Printf("%vkg is %vlb.\n", kilograms, kilograms*2.205)
+			case "pounds":
+				pounds := *poundsFlag
+				fmt.Printf("%vlb is %vkg.\n", pounds, pounds/2.205)
 			}
 		})
 	}
