4a2a225f68bc85b7a59ecbb071bbba7b3ec5868a
James Lucktaylor <jlucktay+github@gmail.com>
2019-08-20 18:04:40 +0100

    Complete 4.9.

---
+++ workspaces/jlucktay/ch4/ex4_9/README.md
@@ -0,0 +1,4 @@
+# Exercise 4.9
+
+Write a program `wordfreq` to report the frequency of each word in an input text file. Call
+`input.Split(bufio.ScanWords)` before the first call to `Scan` to break the input into words instead of lines.
---
+++ workspaces/jlucktay/ch4/ex4_9/main.go
@@ -0,0 +1,27 @@
+// Write a program `wordfreq` to report the frequency of each word in an input text file. Call
+// `input.Split(bufio.ScanWords)` before the first call to `Scan` to break the input into words instead of lines.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int) // counts of words
+
+	in := bufio.NewScanner(os.Stdin)
+	in.Split(bufio.ScanWords)
+
+	for in.Scan() {
+		word := in.Text()
+
+		counts[word]++
+	}
+
+	fmt.Printf("%-40s count\n", "word")
+	for word, count := range counts {
+		fmt.Printf("%-40s %d\n", word, count)
+	}
+}
