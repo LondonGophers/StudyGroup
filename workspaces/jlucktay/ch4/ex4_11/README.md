69a5128f279023f8294c660da6a0923dc66f3b4b
James Lucktaylor <jlucktay+github@gmail.com>
2019-08-26 17:11:22 +0100

    Generate some boilerplate for 4.11 with Cobra.

---
+++ workspaces/jlucktay/ch4/ex4_11/README.md
@@ -0,0 +1,4 @@
+# Exercise 4.11
+
+Build a tool that lets users create, read, update, and close GitHub issues from the command line, invoking their
+preferred text editor when substantial text input is required.
---
+++ workspaces/jlucktay/ch4/ex4_11/main.go
@@ -0,0 +1,10 @@
+// Build a tool that lets users create, read, update, and close GitHub issues from the command line, invoking their
+// preferred text editor when substantial text input is required.
+
+package main
+
+import "github.com/LondonGophers/StudyGroup/workspaces/jlucktay/ch4/ex4_11/cmd"
+
+func main() {
+	cmd.Execute()
+}
