1121cc53b8c86a5f9741cde97d68c1f8d8aad1ba
melina <melina@github.com>
2019-08-26 19:28:11 +0100

    Solve ex2.3

---
+++ workspaces/melina/exercises/ch2/ex2_3/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+// pc[i] is the population count of i.
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func main() {
+	fmt.Println(PopCount(6400))
+	fmt.Println(PopCount2(6400))
+}
+
+// PopCount returns the population count (number of set bits) of x.
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+func PopCount2(x uint64) int {
+	var count int
+	for i := 0; i < 8; i++ {
+		count = count + int(pc[byte(x>>(uint(i)*8))])
+	}
+	return count
+}
