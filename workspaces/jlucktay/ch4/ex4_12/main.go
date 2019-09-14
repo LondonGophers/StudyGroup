d7da904539a8f8fb3bed29e6a77a89e82c5cbb17
James Lucktaylor <jlucktay+github@gmail.com>
2019-09-14 08:03:38 +0100

    Complete 4.12.

--- workspaces/jlucktay/ch4/ex4_12/main.go
+++ workspaces/jlucktay/ch4/ex4_12/main.go
@@ -1,46 +1,67 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
+	fmt.Printf("Search terms: %s\n", os.Args[1:])
+
 	wd, errWD := os.Getwd()
 	if errWD != nil {
 		panic(errWD)
 	}
 
 	xkcdJSONPath := filepath.Join(wd, "xkcd")
-	fmt.Printf("walking '%s'...\n", xkcdJSONPath)
-
 	files, err := ioutil.ReadDir(xkcdJSONPath)
 	if err != nil {
 		panic(err)
 	}
 
-	counter := 0
-
-	// transcripts := make(map[int]string)
+	transcripts := make(map[int]string)
 
 	for _, file := range files {
-		if counter > 10 {
-			os.Exit(0)
+		if file.IsDir() || !strings.HasSuffix(file.Name(), ".json") {
+			fmt.Printf("'%s' is not a JSON file, skipping!", file.Name())
+			continue
 		}
 
 		jsonFilePath := filepath.Join(xkcdJSONPath, file.Name())
-
-		fmt.Println(jsonFilePath)
-		counter++
-
 		content, err := ioutil.ReadFile(jsonFilePath)
 		if err != nil {
+			fmt.Println("File:", file.Name())
 			panic(err)
 		}
 
-		fmt.Printf("File contents: %s", content)
+		comic := new(XKCDComic)
+		if errUm := json.Unmarshal([]byte(content), &comic); errUm != nil {
+			fmt.Println("Content:", content)
+			panic(errUm)
+		}
+
+		transcripts[comic.Num] = comic.Transcript
 	}
 
+	fmt.Printf("# of transcripts: %#v\n", len(transcripts))
+
+	for num, transcript := range transcripts {
+		for _, needle := range os.Args[1:] {
+			lTranscript := strings.ToLower(transcript)
+			lNeedle := strings.ToLower(needle)
+
+			if strings.Contains(lTranscript, lNeedle) {
+				fmt.Printf("https://xkcd.com/%d/\n", num)
+			}
+		}
+	}
+}
+
+type XKCDComic struct {
+	Num        int    `json:"num"`
+	Transcript string `json:"transcript"`
 }
