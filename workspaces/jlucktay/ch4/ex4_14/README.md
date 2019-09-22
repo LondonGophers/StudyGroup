97ccdaab4ed1dadec45e58420e0414c0269fd8b2
James Lucktaylor <jlucktay+github@gmail.com>
2019-09-22 06:20:53 +0100

    4.14: working starter for ten.

---
+++ workspaces/jlucktay/ch4/ex4_14/README.md
@@ -0,0 +1,4 @@
+# Exercise 4.14
+
+Create a web server that queries GitHub once and then allows navigation of the list of bug reports, milestones, and
+users.
---
+++ workspaces/jlucktay/ch4/ex4_14/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/LondonGophers/StudyGroup/workspaces/jlucktay/pkg/github"
+)
+
+func main() {
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("HandleFunc: %s\n", r.URL)
+		searchGitHub(w, r)
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func searchGitHub(out io.Writer, r *http.Request) {
+	if errParse := r.ParseForm(); errParse != nil {
+		panic(errParse)
+	}
+
+	var ghisr []*github.IssueSearchResult
+	searchTerms := r.Form["q"]
+
+	if len(searchTerms) > 0 {
+		ghisr = search(searchTerms)
+	}
+
+	if errExec := template.Must(template.ParseFiles("get.gohtml")).Execute(out, ghisr); errExec != nil {
+		panic(errExec)
+	}
+}
+
+func search(searchTerms []string) []*github.IssueSearchResult {
+	issues, err := github.SearchIssues(searchTerms)
+	if err != nil {
+		panic(err)
+	}
+	return issues.Items
+}
