7a7cfb154935309f407750e10f5adaeb5f5c3dc8
James Lucktaylor <jlucktay+github@gmail.com>
2019-09-25 13:42:45 +0100

    5.1: drop commented block.

--- workspaces/jlucktay/ch5/ex5_1/main.go
+++ workspaces/jlucktay/ch5/ex5_1/main.go
@@ -36,33 +36,3 @@ func visit(links []string, n *html.Node) []string {
 	}
 	return links
 }
-
-/*
-//!+html
-package html
-
-type Node struct {
-	Type                    NodeType
-	Data                    string
-	Attr                    []Attribute
-	FirstChild, NextSibling *Node
-}
-
-type NodeType int32
-
-const (
-	ErrorNode NodeType = iota
-	TextNode
-	DocumentNode
-	ElementNode
-	CommentNode
-	DoctypeNode
-)
-
-type Attribute struct {
-	Key, Val string
-}
-
-func Parse(r io.Reader) (*Node, error)
-//!-html
-*/
