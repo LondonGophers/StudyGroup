9f091dc1aade39b43a3b7942668300f5f73ff589
betandr <bet@andr.io>
2019-12-18 21:42:59 +0000

    ex8_3 wip

---
+++ workspaces/betandr/andr.io/ch8/ex8_3/main.go
@@ -0,0 +1,37 @@
+// In `netcat3`, the interface value `conn` has the concrete type `*net.TCPConn`,
+// which represents a TCP connection. A TCP connection consists of two halves that
+// may be closed independently using it's `CloseRead` and `CloseWrite` methods.
+// Modify the main goroutine of `netcat3` to close only the write half of the
+// connection so that the program will continue to print the final echoes from the
+// `reverb1` server even after the standard input has been closed. (Doing this for
+// the `reverb` server is harder; see Exercise 8.4.)
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn) // NOTE: ignoring errors
+		log.Println("done")
+		done <- struct{}{} // signal the main goroutine
+	}()
+	mustCopy(conn, os.Stdin)
+	conn.Close()
+	<-done // wait for background goroutine to finish
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
