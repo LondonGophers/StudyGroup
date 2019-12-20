3af06fb67815e1826ff735d76e972889d39e20b6
Beth Anderson <bet@andr.io>
2019-12-20 17:31:40 +0000

    ex8_4 wip

---
+++ workspaces/betandr/andr.io/ch8/ex8_4/main.go
@@ -0,0 +1,47 @@
+// Modify the `reverb2` server to use a `sync.WaitGroup` per connection to count
+// the number of active echo goroutines. When it falls to zero, close the write
+// half of the TCP connection as described in Exercise 8.3. Verify that your
+// modified `netcat3` client from that exercise waits for the final echoes of
+// multiple concurrent shouts, even after the standard input has been closed.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+)
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		go echo(c, input.Text(), 1*time.Second)
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	c.Close()
+}
+
+func main() {
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+}
