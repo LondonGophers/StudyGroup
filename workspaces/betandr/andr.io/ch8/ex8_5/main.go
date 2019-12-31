e9990078bfebb525c28e20152765e71962318cf8
Beth Anderson <bet@andr.io>
2019-12-31 15:26:51 +0000

    ex8_5 wip

---
+++ workspaces/betandr/andr.io/ch8/ex8_5/main.go
@@ -0,0 +1,53 @@
+// Take an existing CPU-bound sequential program, such as the Mandelbrot program of
+// Section 3.3 of the 3D surface computation of Section 3.2, and execute its main
+// loop in parallel using channels for communication. How much faster does it run
+// on a multiprocessor machine? What is the optimal number of goroutines to use?
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math"
+	"math/cmplx"
+	"os"
+)
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+
+	png.Encode(os.Stdout, img)
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			switch {
+			case n > 50:
+				return color.RGBA{0, 255, 0, 255}
+			default:
+				logScale := math.Log(float64(n)) / math.Log(float64(iterations))
+				return color.RGBA{0, 0, 255 - uint8(logScale*255), 255}
+			}
+		}
+	}
+	return color.Black
+}
--- workspaces/betandr/go.mod
+++ workspaces/betandr/go.mod
@@ -2,4 +2,4 @@ module andr.io
 
 go 1.12
 
-require gopl.io v0.0.0-20161206225729-b725d6015f98 // indirect
+require gopl.io v0.0.0-20161206225729-b725d6015f98
