a130025012cb9d060134bfe2ae24c7e21dd1842a
betandr <bet@andr.io>
2020-01-15 17:26:03 +0000

    ex8_6 wip

--- workspaces/betandr/andr.io/ch8/ex8_6/main.go
+++ workspaces/betandr/andr.io/ch8/ex8_6/main.go
@@ -8,53 +8,74 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
+	"sync"
 
 	"gopl.io/ch5/links"
 )
 
 var depth = flag.Int("depth", 3, "Only URLs reachable by this number of links will be fetched")
+var url = flag.String("url", "", "The URL to start crawling from.")
 
 type link struct {
 	URL   string
 	Depth int
 }
 
-func crawl(url link) []string {
+func crawl(url link) []link {
 	fmt.Println(url)
 	list, err := links.Extract(url.URL)
 	if err != nil {
 		log.Print(err)
 	}
-	return list
+
+	links := make([]link, len(list))
+	for i, l := range list {
+		links[i] = link{URL: l, Depth: url.Depth}
+	}
+	return links
 }
 
 func main() {
-	worklist := make(chan []string) // lists of URLs, may have duplicates
-	unseenLinks := make(chan link)  // de-duplicated URLs
+	worklist := make(chan []link)  // lists of URLs, may have duplicates
+	unseenLinks := make(chan link) // de-duplicated URLs
+	var wg sync.WaitGroup
 
-	// Add command-line arguments to worklist.
-	go func() { worklist <- os.Args[1:] }()
+	flag.Parse()
+
+	// Start from specified url
+	start := make([]link, 1)
+	start[0] = link{URL: *url, Depth: 0}
+	go func() { worklist <- start }()
 
 	// Create 20 crawler goroutines to fetch each unseen link.
 	for i := 0; i < 20; i++ {
 		go func() {
-			for link := range unseenLinks {
-				foundLinks := crawl(link)
-				// increment depth here
-				go func() { worklist <- foundLinks }()
+			for unseenLink := range unseenLinks {
+				go func(l link) {
+					if l.Depth <= *depth {
+						foundLinks := crawl(l)
+						worklist <- foundLinks
+					}
+				}(unseenLink)
 			}
 		}()
 	}
+
+	// wait for done and close work channel
+	go func() {
+		wg.Wait()
+		close(worklist)
+	}()
 
 	// The main goroutine de-duplicates worklist items
 	// and sends the unseen ones to the crawlers.
 	seen := make(map[string]bool)
 	for list := range worklist {
 		for _, l := range list {
-			if !seen[l] {
-				seen[l] = true
-				unseenLinks <- link{l, 0}
+			if !seen[l.URL] {
+				seen[l.URL] = true
+				wg.Add(1)
+				unseenLinks <- link{l.URL, l.Depth + 1}
 			}
 		}
 	}
