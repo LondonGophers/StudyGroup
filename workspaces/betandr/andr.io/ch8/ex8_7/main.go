13c8f076ed54bfd4b05263e0bf3a5979ad9f28e3
betandr <bet@andr.io>
2020-03-13 10:34:23 +0000

    ex8_7 wip

---
+++ workspaces/betandr/andr.io/ch8/ex8_7/main.go
@@ -0,0 +1,101 @@
+// Write a concurrent program that creates a local mirror of a website, fetching
+// each reachable page and writing it to a director on the local disk. Only pages
+// within the original domain (for instance `golang.org`) should be fetched. URLs
+// within mirrored pages should be altered as needed so that they refer to the
+// mirrored page, not the original.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"gopl.io/ch5/links"
+)
+
+var url = flag.String("url", "", "The URL to mirror.")
+
+type link struct {
+	URL   string
+	Depth int
+}
+
+func crawl(url link) []link {
+	fmt.Println(url.URL)
+	list, err := links.Extract(url.URL)
+	if err != nil {
+		log.Print(err)
+	}
+
+	links := make([]link, len(list))
+	for i, l := range list {
+		links[i] = link{URL: l, Depth: url.Depth}
+	}
+	return links
+}
+
+func main() {
+	worklist := make(chan []link)  // lists of URLs, may have duplicates
+	unseenPages := make(chan link) // de-duplicated URLs
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	flag.Parse()
+
+	// Start from specified url
+	start := make([]link, 1)
+	start[0] = link{URL: *url, Depth: 0}
+	go func() { worklist <- start }()
+
+	// Create 20 crawler goroutines to fetch each unseen link.
+	for i := 0; i < 20; i++ {
+		go func() {
+			for unseenPage := range unseenPages {
+				go func(l link) {
+					if l.Depth <= 1 {
+						foundLinks := crawl(l)
+						worklist <- foundLinks
+					}
+					wg.Done()
+				}(unseenPage)
+			}
+		}()
+	}
+
+	// wait for done and close work channel
+	go func() {
+		wg.Wait()
+		close(worklist)
+	}()
+
+	// this feels like a kludge; because of the wg.Add(1) at the start
+	// that gives the goroutines time to start adding waitgroup items
+	// before the worklist channel is closed we need to remove this
+	// waitgroup item. So, this goroutine sleeps for a couple of seconds,
+	// to give the others time to start working, then removes the "guard"
+	// goroutine.
+	go func() {
+		time.Sleep(2 * time.Second)
+		wg.Done()
+	}()
+
+	// The main goroutine de-duplicates worklist items
+	// and sends the unseen ones to the crawlers.
+
+	// if depth == 1
+	// if path not seen
+	// send to unseenPages
+	seen := make(map[string]bool)
+	for list := range worklist {
+		for _, l := range list {
+			fmt.Println("----> ", l)
+			if !seen[l.URL] {
+				seen[l.URL] = true
+				unseenPages <- link{l.URL, l.Depth + 1}
+				wg.Add(1)
+			}
+		}
+	}
+}
