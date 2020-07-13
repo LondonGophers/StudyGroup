5ffc00a76bff311da4c35b964f49beae3ba0f4b2
Beth Anderson <bet@andr.io>
2020-07-13 13:31:19 +0100

    ex8_7

--- workspaces/betandr/andr.io/ch8/ex8_7/main.go
+++ workspaces/betandr/andr.io/ch8/ex8_7/main.go
@@ -1,14 +1,21 @@
 // Write a concurrent program that creates a local mirror of a website, fetching
-// each reachable page and writing it to a director on the local disk. Only pages
+// each reachable page and writing it to a directory on the local disk. Only pages
 // within the original domain (for instance `golang.org`) should be fetched. URLs
 // within mirrored pages should be altered as needed so that they refer to the
 // mirrored page, not the original.
+//
+// This program is pretty brittle and doesn't really work that well but it does
+// explore the ideas of using channels to do some work so I'll leave it there for now.
 package main
 
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -16,6 +23,7 @@ import (
 )
 
 var url = flag.String("url", "", "The URL to mirror.")
+var out = flag.String("out", "", "The location to write mirror to.")
 
 type link struct {
 	URL   string
@@ -34,6 +42,36 @@ func crawl(url link) []link {
 		links[i] = link{URL: l, Depth: url.Depth}
 	}
 	return links
+}
+
+// domain in general cases returns the domain name from a URL
+// but also is very brittle so won't stand up to much use.
+// Given the string:
+// https://www.example.com/something/else.html
+// ...it will return www.example.com
+func domain(link string) string {
+	return strings.Split(link, "/")[2]
+}
+
+// download a link given a path
+func download(l link, path string) error {
+	response, err := http.Get(l.URL)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	u := strings.Split(l.URL, "/")
+	filename := fmt.Sprintf("%s/%s", path, u[len(u)-1])
+
+	out, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, response.Body)
+	return err
 }
 
 func main() {
@@ -90,7 +128,13 @@ func main() {
 	seen := make(map[string]bool)
 	for list := range worklist {
 		for _, l := range list {
-			fmt.Println("----> ", l)
+			if strings.Contains(domain(l.URL), domain(*url)) {
+				fmt.Println("downloading: ", l.URL)
+				download(l, *out)
+			} else {
+				fmt.Println("ignoring: ", l.URL)
+			}
+
 			if !seen[l.URL] {
 				seen[l.URL] = true
 				unseenPages <- link{l.URL, l.Depth + 1}
