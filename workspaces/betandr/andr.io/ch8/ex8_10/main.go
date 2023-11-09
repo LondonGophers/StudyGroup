d5d099beb92f1bfca887209a52e76f39e8812447
betandr <bet@andr.io>
2023-11-09 12:13:48 +0000

    ex8_10 wip

---
+++ workspaces/betandr/andr.io/ch8/ex8_10/main.go
@@ -0,0 +1,101 @@
+// HTTP requests may be cancelled by closing the optional `Cancel` channel in the
+// `http.Request` struct. Modify the web crawler of Section 8.6 to support
+// cancellation.
+
+// Hint: the `http.Get` convenience function does not give you an opportunity to
+// customize a `Request`. Instead, create the request using `http.NewRequest`, set
+// its `Cancel` field, then perform the request by calling
+// `http.DefaultClient.Do(req)`.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"sync"
+	"time"
+
+	"gopl.io/ch5/links"
+)
+
+var depth = flag.Int("depth", 3, "Only URLs reachable by this number of links will be fetched")
+var url = flag.String("url", "", "The URL to start crawling from.")
+
+type link struct {
+	URL   string
+	Depth int
+}
+
+func crawl(url link) []link {
+	fmt.Println(url.URL)
+	list, err := links.Extract(url.URL)
+	if err != nil {
+		log.Print(err)
+	}
+
+	links := make([]link, len(list))
+	for i, l := range list {
+		links[i] = link{URL: l, Depth: url.Depth}
+	}
+	return links
+}
+
+func main() {
+	worklist := make(chan []link)  // lists of URLs, may have duplicates
+	unseenLinks := make(chan link) // de-duplicated URLs
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	flag.Parse()
+
+	// Start from specified url
+	start := make([]link, 1)
+	start[0] = link{URL: *url, Depth: 0}
+	go func() { worklist <- start }()
+
+	// Create 20 crawler goroutines to fetch each unseen link.
+	for i := 0; i < 20; i++ {
+		go func() {
+			for unseenLink := range unseenLinks {
+				go func(l link) {
+					if l.Depth <= *depth {
+						foundLinks := crawl(l)
+						worklist <- foundLinks
+					}
+					wg.Done()
+				}(unseenLink)
+			}
+		}()
+	}
+
+	// wait for done and close work channel
+	go func() {
+		wg.Wait()
+		close(worklist)
+	}()
+
+	// this feels like a kludge; because of the wg.Add(1) at the start
+	// that gives the goroutines time to start adding waitgroup items
+	// before the worklist channel is closed we need to remove this
+	// waitgroup item. So, this goroutine sleeps for a couple of seconds,
+	// to give the others time to start working, then removes the "guard"
+	// goroutine.
+	go func() {
+		time.Sleep(2 * time.Second)
+		wg.Done()
+	}()
+
+	// The main goroutine de-duplicates worklist items
+	// and sends the unseen ones to the crawlers.
+	seen := make(map[string]bool)
+	for list := range worklist {
+		for _, l := range list {
+			if !seen[l.URL] {
+				seen[l.URL] = true
+				unseenLinks <- link{l.URL, l.Depth + 1}
+				wg.Add(1)
+			}
+		}
+	}
+}
--- workspaces/betandr/andr.io/go.mod
+++ workspaces/betandr/andr.io/go.mod
@@ -2,7 +2,6 @@ module andr.io
 
 go 1.19
 
-require (
-	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6
-	gopl.io v0.0.0-20211004154805-1ae3ec64947b
-)
+require gopl.io v0.0.0-20211004154805-1ae3ec64947b
+
+require golang.org/x/net v0.18.0 // indirect
